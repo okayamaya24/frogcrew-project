d2b60c868236fd60f3a229b49f704703b64b1b59
okayamaya24 <amaya.harris@tcu.edu>
2025-04-23 16:54:35 -0500

    Delete backend/src/main/java/edu.tcu.cs.frogcrewbackend directory

--- backend/src/main/java/edu.tcu.cs.frogcrewbackend/availability/AvailabilityRepository.java
+++
@@ -1,6 +0,0 @@
-package edu.tcu.cs.frogcrewbackend.availability;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvailabilityRepository extends JpaRepository<Availability, Integer> {
-}
--- backend/src/main/java/edu.tcu.cs.frogcrewbackend/crewmember/CrewMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.tcu.cs.frogcrewbackend.crewmember;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CrewMemberRepository extends JpaRepository<CrewMember, Integer> {
-}
